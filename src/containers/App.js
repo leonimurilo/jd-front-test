bc7ea43fac13d17729023ddbc54000683564b4e8
leoni <leonimega@hotmail.com>
2021-03-09 20:22:58 -0300

    add baseui as theme provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,29 @@
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { createTheme, BaseProvider } from "baseui";
 import CreateDoc from "./CreateDoc";
 import List from "./List";
 
+const primitives = {
+  accent: "#367C2B",
+};
+
+const theme = createTheme(primitives, {});
+
+const engine = new Styletron();
+
 function App() {
   return (
-    <div className="App">
-      <h2>Create</h2>
-      <CreateDoc />
-      <h2>List</h2>
-      <List />
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={theme}>
+        <div className="App">
+          <h2>Create</h2>
+          <CreateDoc />
+          <h2>List</h2>
+          <List />
+        </div>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
