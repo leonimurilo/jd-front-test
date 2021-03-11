c40f628d8ab30ef2b7f4d46608ad5b38f6cf9891
leoni <leonimega@hotmail.com>
2021-03-10 21:18:43 -0300

    improve validation

--- src/containers/CreateDoc/validations.js
+++ src/containers/CreateDoc/validations.js
@@ -2,14 +2,14 @@ import * as Yup from "yup";
 
 export const CreateDocValidation = Yup.object().shape({
   anomes: Yup.string().required("Required"),
-  ibge: Yup.string().required("Required"),
+  ibge: Yup.string().required("Required").max(7),
   siglauf: Yup.mixed().test("uf-test", "Required", (value) => {
     return value && Array.isArray(value) && value[0] && value[0].id;
   }),
-  qtd_ben_bas: Yup.number().required("Required"),
-  qtd_ben_var: Yup.number().required("Required"),
-  qtd_ben_bvj: Yup.number().required("Required"),
-  qtd_ben_bvn: Yup.number().required("Required"),
-  qtd_ben_bvg: Yup.number().required("Required"),
-  qtd_ben_bsp: Yup.number().required("Required"),
+  qtd_ben_bas: Yup.number().min(0).required("Required"),
+  qtd_ben_var: Yup.number().min(0).required("Required"),
+  qtd_ben_bvj: Yup.number().min(0).required("Required"),
+  qtd_ben_bvn: Yup.number().min(0).required("Required"),
+  qtd_ben_bvg: Yup.number().min(0).required("Required"),
+  qtd_ben_bsp: Yup.number().min(0).required("Required"),
 });
